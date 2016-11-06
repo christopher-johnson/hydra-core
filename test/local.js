a6af9db9d86178f7d94fbca4fa9bd6cba8403f8e
Christopher Johnson <christopher.johnson@wikimedia.de>
2016-11-06 12:05:33 +0100

    implements uri-templates for IriTemplate parsing

--- test/local.js
+++ test/local.js
@@ -9,7 +9,8 @@ var dontDelete = false;
 
 var
     hydra = require('../'),
-    jsonldp = require('jsonld').promises();
+    jsonldp = require('jsonld').promises(),
+    uriTemplates = require('uri-templates');
 
 var config = {
     base: 'http://localhost:3000',
@@ -19,21 +20,26 @@ var config = {
 };
 
 var ns = {
-    graphs: 'http://service.b-ol.de/api/hydra.json#EntryPoint/api/graphs'
+    Manifest: 'http://iiif.io/api/presentation/2#Manifest'
+};
+
+var manifest = {
+    '@type': ns.Manifest,
+    node: 'edition/0097'
 };
 
 Promise.resolve()
     .then(function () {
         return hydra.loadDocument(config.base)
             .then(function (document) {
-                var graphs = document.findOperation(ns.graphs, 'GET').invoke;
-                return graphs()
+                var graphs = document.findOperation(ns.Manifest, 'GET').invoke;
+                var doc = document.findOperation(ns.Manifest, 'GET');
+                var template = uriTemplates(doc.link);
+                var uri1 = template.fill({node : manifest.node});
+                return graphs(uri1)
                     .then(function (response) {
-                        return jsonldp.compact(response, {'@vocab': 'http://www.w3.org/ns/hydra/core#'});
-                    })
-                    .then(function (response) {
-                        graphs = response;
-                        console.log(graphs);
+                        out = response;
+                        console.log(out);
                     })
             });
     })
